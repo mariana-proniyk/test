4c9e3c7b2da63a2322854d5f2ccf441a4ba16022
Mariana Proniuk <meriproo@gmail.com>
2023-03-24 17:28:27 +0200

    feat: add new packages

--- gulpfile.js
+++ gulpfile.js
@@ -49,6 +49,8 @@ function librariesTask(done) {
     src([
         "./node_modules/jquery/dist/jquery.min.js",
         "./node_modules/remodal/dist/remodal.min.js",
+        "./node_modules/jquery-validation/dist/jquery.validate.min.js",
+        "./node_modules/slick-slider/slick/slick.min.js",
     ])
         .pipe(
             plumber({
